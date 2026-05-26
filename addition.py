cff0db82556de645477d2706bbafef031661f778
ttejaskumar25-coder <ttejaskumar25@gmail.com>
2026-05-26 16:14:49 +0530

    Add Python addition utility

---
+++ addition.py
@@ -0,0 +1,9 @@
+def add_two_numbers(a, b):
+    """Return the sum of two numbers."""
+    return a + b
+
+
+if __name__ == "__main__":
+    x = 3
+    y = 5
+    print(f"The sum of {x} and {y} is {add_two_numbers(x, y)}")
